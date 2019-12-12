ddb3e22e74f8895d5cda0c00a23e1734edb931fa
klarakuc <klarakuc.volny.cz>
2019-12-12 14:00:16 +0000

    comments

---
+++ 002_Transform.R
@@ -0,0 +1,6 @@
+#Transform
+#During data transformation step we might need to
+#transform numeric variables using the log as required.
+#transform factors into numeric variables (and vice versa) as necessary.
+
+
