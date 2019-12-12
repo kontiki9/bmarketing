5ec0dd55054106b5ea216b494a1fb33c99aef032
kontiki9 <pawel.sobala@uniqa.at>
2019-12-12 15:53:25 +0000

    style changes to the functions

--- 003_modelling.R
+++ 003_modelling.R
@@ -1,10 +1,12 @@
 # Model: Create a decision tree model to predict if customer signing a term deposit.
 
-model <- function(input_data,res_var){
+model <- function(input_data,
+                  res_var){
   rpart(res_var ~ ., data = input_data)
 }
 
-dt_model<- model(bmarketing,bmarketing$y)
+# EXAMPLE:
+# dt_model<- model(bmarketing,bmarketing$y)
 
 # Model-Plot: 
 # We shall implement a function to present a nice representation of the model, 
@@ -14,23 +16,27 @@ plot_model <- function(model_res){
   rpart.plot(model_res)  
 }
 
-plot_model(dt_model)
-summary(dt_model)
+# EXAMPLE:
+# plot_model(dt_model)
+# summary(dt_model)
 
 
 # Model-Predict: 
 # The model prediction function shall return the actual predicted classes from the model.
 
-predict_model <- function(model_res, input_data){
+predict_model <- function(model_res, 
+                          input_data){
   predict(model_res, input_data, type = "class")
 }
 
-predictions <- predict_model(dt_model, bmarketing)
+# EXAMPLE:
+# predictions <- predict_model(dt_model, bmarketing)
 
 # Model-Performance: 
 # The model performance function should calculate the accuracy (or precision) of the model.
 
-test_model_performance <- function(input_model, pred){
+test_model_performance <- function(input_model, 
+                                   pred){
   ## Compute the accuracy
   a <- mean(pred$y == input_model)
   
@@ -40,6 +46,8 @@ test_model_performance <- function(input_model, pred){
   list(acc=a, conf_mat =b)
 }
 
-test_model_performance(predictions, bmarketing)
+# EXAMPLE:
+# test_model_performance(predictions, 
+#                         bmarketing)
 
 
